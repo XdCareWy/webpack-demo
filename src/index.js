636438e25f880c42625f968784d3f08d73db3fc5
XdCareWy <aixu159@163.com>
2017-08-01 19:01:52 +0800

    add loader for image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './4.jpg';
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.width = 100;
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
