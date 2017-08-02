98e6d12dfd0a2b2eb7160ecadb561482f89ba006
XdCareWy <aixu159@163.com>
2017-08-02 11:02:03 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './4.jpg';
-import Data from './data.xml';
-import dd from './data.json';
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var myIcon = new Image();
-  myIcon.width = 100;
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(dd);
 
   return element;
 }
