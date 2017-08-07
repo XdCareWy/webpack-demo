2f1649bec96f82d8a4de0ad420449ef1178f9de6
XdCareWy <aixu159@163.com>
2017-08-07 15:42:40 +0800

    lazy-loading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+import _ from "lodash";
 
-function getComponent() {
-	return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-		var element = document.createElement('div');
+function component() {
+	var element = document.createElement('div');
+	element.innerHTML = _.join(['Hello', 'webpack'], '');
+	var br = document.createElement('br');
+	var button = document.createElement('button');
+	button.innerHTML = "Click me and look at the console!";
+	element.appendChild(br);
+	element.appendChild(button);
 
-		element.innerHTML = _.join(['Hello', 'webpack'], '');
+	button.onclick = e => import(/* webpackChunkName: "print" */ './print.js').then(module => {
+		var print = module.default;
 
-		return element;
-	}).catch(error => 'An error occurred while loading thie component');
+		print();
+	});
+	return element;
 }
 
-getComponent().then(component => {
-	document.body.appendChild(component);
-})
+document.body.appendChild(component());
+
 
